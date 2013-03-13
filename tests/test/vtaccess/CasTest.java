35eef9c2d72e868a4ba5405e71bb46bbfdb57bbf
egaebel <egaebel@vt.edu>
2013-03-13 00:59:39 -0400

    removed test stuff

---
+++ tests/test/vtaccess/CasTest.java
@@ -0,0 +1,233 @@
+package test.vtaccess;
+
+import java.util.LinkedList;
+import java.util.List;
+import com.vtaccess.Cas;
+import com.vtaccess.ScheduleScraper;
+import com.vtaccess.Semester;
+import com.vtaccess.exceptions.HokieSpaTimeoutException;
+import com.vtaccess.exceptions.WrongLoginException;
+import com.vtaccess.schedule.Course;
+import com.vtaccess.schedule.Schedule;
+import junit.framework.TestCase;
+
+
+/**
+ * TO RUN THIS TEST YOU MUST PLACE A VALID HOKIESPA USERNAME AND PASSWORD IN THE APPROPRIATE FIELDS BELOW
+ */
+public class CasTest extends TestCase {
+
+    //~Data Fields--------------------------------------------
+    private Schedule emptySchedule;
+
+    //~Constructors--------------------------------------------
+    protected void setUp() throws Exception {
+
+        super.setUp();
+        emptySchedule = new Schedule();
+    }
+
+    //~Methods--------------------------------------------
+    public void testSwitchUsersNOFILEPATH() throws WrongLoginException, HokieSpaTimeoutException {
+
+        Cas cas = new Cas(username.toCharArray(), password.toCharArray());
+        ScheduleScraper s = new ScheduleScraper(cas);
+        assertTrue(s.retrieveSchedule(new Schedule(), SPRING_CODE));
+
+        cas.switchUsers(username.toCharArray(), password.toCharArray());
+        assertTrue(s.retrieveSchedule(new Schedule(), SPRING_CODE));
+    }
+
+    public void testSwitchUsersFILEPATH() throws WrongLoginException, HokieSpaTimeoutException {
+
+        Cas cas = new Cas(username.toCharArray(), password.toCharArray(), "");
+        ScheduleScraper s = new ScheduleScraper(cas);
+        assertTrue(s.retrieveSchedule(new Schedule(), SPRING_CODE));
+
+        cas.switchUsers(username.toCharArray(), password.toCharArray(), "");
+        assertTrue(s.retrieveSchedule(new Schedule(), SPRING_CODE));
+    }
+
+    public void testMultipleLogins() throws WrongLoginException, HokieSpaTimeoutException {
+        System.out.println("TEST MULTIPLE LOGINS");
+        Cas cas = null;
+        cas = new Cas(username.toCharArray(), password.toCharArray());
+        ScheduleScraper s = new ScheduleScraper(cas);
+
+        Schedule s1 = new Schedule();
+        Schedule s2 = new Schedule();
+        Schedule s3 = new Schedule();
+        assertTrue(s.retrieveSchedule(s1, SPRING_CODE));
+        assertTrue(s.retrieveSchedule(s2, FALL_CODE));
+
+
+        assertTrue(s.retrieveSchedule(s3, FALL_CODE));
+        assertNotSame(s3.getAllCourses().toString(), "");
+        assertEquals(s3.getAllCourses().toString(), s2.getAllCourses().toString());
+        s3 = new Schedule();
+
+        assertTrue(s.retrieveSchedule(s3, Semester.nextSemesterCode(FALL_CODE)));
+        assertNotSame(s3.getAllCourses().toString(), "");
+        assertEquals(s3.getAllCourses().toString(), s1.getAllCourses().toString());
+        s3 = new Schedule();
+
+        assertTrue(s.retrieveSchedule(s3, FALL_CODE));
+        assertNotSame(s3.getAllCourses().toString(), "");
+        assertEquals(s3.getAllCourses().toString(), s2.getAllCourses().toString());
+
+
+
+
+        System.out.println(s1.getAllCourses().toString());
+        System.out.println(s2.getAllCourses().toString());
+        assertNotSame(s1.getAllCourses().toString(), "");
+        assertNotSame(s2.getAllCourses().toString(), "");
+
+        //Finals stuff
+        List<Course> finalsList1 = new LinkedList<Course>();
+        List<Course> finalsList2 = new LinkedList<Course>();
+        assertTrue(s.retrieveExamSchedule(SPRING_CODE, finalsList1));
+        assertTrue(finalsList1.size() > 0);
+        System.out.println("retrieve exam schedule 1 RESULTES!!!\n" + finalsList1.toString());
+        assertTrue(s.retrieveExamSchedule(SPRING_CODE, finalsList2));
+        assertTrue(finalsList2.size() > 0);
+        System.out.println("retrieve exam schedule 2 RESULTES!!!\n" + finalsList2.toString());
+
+
+        System.out.println(finalsList1.toString());
+        System.out.println(finalsList2.toString());
+    }
+
+
+    public void testLoginNoPath() throws HokieSpaTimeoutException {
+        System.out.println("TEST LOGIN, NO PATH");
+
+        Cas cas = null;
+        ScheduleScraper scheduleScraper = null;
+
+
+        try {
+            cas = new Cas(username.toCharArray(), password.toCharArray());
+            scheduleScraper = new ScheduleScraper(cas);
+        }
+        catch (WrongLoginException e) {
+            e.printStackTrace();
+            System.out.println("ERROR!!!!");
+        }
+
+        Schedule schedule = new Schedule();
+        String semester = "201209";
+
+        scheduleScraper.retrieveSchedule(schedule, semester);
+
+        assertTrue(!schedule.toXML().equals(emptySchedule.toXML()));
+
+        assertTrue(cas.isActive());
+
+        cas.closeSession();
+
+        assertFalse(cas.isActive());
+
+        assertFalse(scheduleScraper.retrieveSchedule(schedule, semester));
+    }
+
+    public void testLoginPath() throws HokieSpaTimeoutException {
+        System.out.println("TEST LOGIN, WITH PATH");
+        Cas cas = null;
+        ScheduleScraper scheduleScraper = null;
+
+        try {
+            cas = new Cas(username.toCharArray(), password.toCharArray(), "");
+            scheduleScraper = new ScheduleScraper(cas);
+        }
+        catch (WrongLoginException e) {
+            e.printStackTrace();
+            System.out.println("ERROR!!!!");
+        }
+
+        Schedule schedule = new Schedule();
+        String semester = "201209";
+
+        scheduleScraper.retrieveSchedule(schedule, semester);
+
+        assertTrue(!schedule.toXML().equals(emptySchedule.toXML()));
+
+        assertTrue(cas.isActive());
+
+        cas.closeSession();
+
+        assertFalse(cas.isActive());
+
+        scheduleScraper.retrieveSchedule(schedule, semester);
+    }
+
+    public void testWrongLoginExceptionNoPath() {
+        System.out.println("TEST WRONG LOGIN EXCEPTION NO PATH");
+        WrongLoginException except = null;
+        @SuppressWarnings("unused")
+        Cas cas;
+
+        //test wrong username
+        try {
+            cas = new Cas("egsadasdelasdasd".toCharArray(), password.toCharArray());
+        }
+        catch (WrongLoginException e) {
+
+            except = e;
+        }
+
+        assertNotNull(except);
+
+        except = null;
+
+        //test wrong password
+        try {
+            cas = new Cas(username.toCharArray(), "hjkl;lkl;lk".toCharArray());
+        }
+        catch (WrongLoginException e) {
+
+            except = e;
+        }
+
+        assertNotNull(except);
+    }
+
+    public void testWrongLoginExceptionPath() {
+        System.out.println("TEST WRONG LOGIN EXCEPTION PATH");
+        @SuppressWarnings("unused")
+        Cas cas;
+        WrongLoginException except = null;
+
+        //test wrong password
+        try {
+            cas = new Cas(username.toCharArray(), "hjkl;lkl;lk".toCharArray(), "");
+        }
+        catch (WrongLoginException e) {
+
+            except = e;
+        }
+
+        assertNotNull(except);
+
+
+        except = null;
+
+        //test wrong username
+        try {
+            cas = new Cas("egsadasdelasdasd".toCharArray(), password.toCharArray(), "");
+        }
+        catch (WrongLoginException e) {
+
+            except = e;
+            System.out.println("exception caught!");
+        }
+
+        assertTrue(except != null);
+    }
+
+    private static final String SPRING_CODE = "201301";
+    private static final String FALL_CODE = "201209";
+    private static final String username = "";
+    private static final String password = "";
+    private static final String filePath = "";
+}
---
+++ tests/test/vtaccess/ScheduleScraperTest.java
@@ -0,0 +1,181 @@
+package test.vtaccess;
+
+import java.util.LinkedList;
+import java.util.List;
+import com.vtaccess.Cas;
+import com.vtaccess.ScheduleScraper;
+import com.vtaccess.exceptions.HokieSpaTimeoutException;
+import com.vtaccess.exceptions.WrongLoginException;
+import com.vtaccess.schedule.Course;
+import com.vtaccess.schedule.Schedule;
+import junit.framework.TestCase;
+
+
+public class ScheduleScraperTest extends TestCase {
+
+    //~Constants----------------------------------------------
+
+
+    //~Data Fields--------------------------------------------
+
+
+    //~Constructors--------------------------------------------
+
+
+    //~Methods-------------------------------------------------
+    /**
+     * TO RUN THIS TEST YOU MUST PLACE A VALID HOKIESPA USERNAME AND PASSWORD IN THE APPROPRIATE FIELDS BELOW
+     */
+    public void testRetrieveSchedule() {
+        System.out.println("-----BEGIN RETRIEVE SCHEDULE TEST-----------");
+        Cas cas = null;
+        ScheduleScraper scheduleScraper = null;
+
+        try {
+            cas = new Cas(username.toCharArray(), password.toCharArray());
+            scheduleScraper = new ScheduleScraper(cas);
+
+            Schedule schedule = new Schedule();
+            Schedule schedule2 = new Schedule();
+            Schedule schedule3 = new Schedule();
+            Schedule schedule4 = new Schedule();
+            Schedule schedule5 = new Schedule();
+            Schedule schedule6 = new Schedule();
+            Schedule schedule7 = new Schedule();
+            Schedule emptySchedule = new Schedule();
+
+            scheduleScraper.retrieveSchedule(schedule, "201209");
+            assertTrue(!schedule.toXML().equals(emptySchedule.toXML()));
+            System.out.println("GENERAL TEST PASSED!");
+
+            //Failure cases
+            assertFalse(scheduleScraper.retrieveSchedule(null, "201209"));
+            assertFalse(scheduleScraper.retrieveSchedule(schedule, null));
+            assertFalse(scheduleScraper.retrieveSchedule(null, null));
+            System.out.println("FAILURE TESTS PASSED!");
+
+            //far out of range case! (schedule 2)
+            assertFalse(scheduleScraper.retrieveSchedule(schedule2, "201409"));
+            assertTrue(schedule2.toXML().equals(emptySchedule.toXML()));
+            System.out.println("FUTURE SEMESTERCODE TEST PASSED!");
+
+            //far before range case (schedule 3)
+            assertFalse(scheduleScraper.retrieveSchedule(schedule3, "200809"));
+            assertTrue(schedule3.toXML().equals(emptySchedule.toXML()));
+            System.out.println("WAY BACK WHEN TEST PASSED!");
+
+            //Enrolled at VT but not semester
+            assertFalse(scheduleScraper.retrieveSchedule(schedule4, "201207"));
+            assertTrue(schedule4.toXML().equals(emptySchedule.toXML()));
+            System.out.println("NON-PARTICIPATING SEMESTER TEST PASSED!");
+
+            //Totally invalid semesterCode (but correct length)
+            assertFalse(scheduleScraper.retrieveSchedule(schedule5, "548766"));
+            assertTrue(schedule5.toXML().equals(emptySchedule.toXML()));
+            System.out.println("INVALID SEMESTER CODE TEST PASSED!");
+
+            //Wrong length semesterCode
+            assertFalse(scheduleScraper.retrieveSchedule(schedule6, "20120999"));
+            assertTrue(schedule6.toXML().equals(emptySchedule.toXML()));
+            System.out.println("TOO LONG SEMESTER CODE TEST PASSED!");
+
+            assertTrue(cas.isActive());
+            //Additional Times course
+            System.out.println("\n\nADDITIONAL TIMES CHECK!!!!!!!!");
+            assertTrue(scheduleScraper.retrieveSchedule(schedule7, "201109"));
+            System.out.println(schedule7.toXML());
+            System.out.println("********************************************************");
+            System.out.println("********************************************************");
+            System.out.println(schedule7.toString());
+        }
+        catch (WrongLoginException e) {
+            e.printStackTrace();
+        }
+        catch (HokieSpaTimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void testRetrieveExamTimes() {
+        System.out.println("-----BEGIN RETRIEVE EXAMS TEST-----------");
+        Cas cas = null;
+        ScheduleScraper scheduleScraper = null;
+
+        try {
+            cas = new Cas(username.toCharArray(), password.toCharArray());
+            scheduleScraper = new ScheduleScraper(cas);
+
+            List<Course> finals1 = new LinkedList<Course>();
+            List<Course> finals2 = new LinkedList<Course>();
+            List<Course> finals3 = new LinkedList<Course>();
+            List<Course> finals4 = new LinkedList<Course>();
+            List<Course> finals5 = new LinkedList<Course>();
+            List<Course> finals6 = new LinkedList<Course>();
+            List<Course> finals7 = new LinkedList<Course>();
+            Course emptyCourse = new Course();
+
+            scheduleScraper.retrieveExamSchedule("201209", finals1);
+            for (Course course : finals1) {
+                assertTrue(!course.toXML().equals(emptyCourse.toXML()));
+            }
+            System.out.println("GENERAL TEST PASSED!");
+
+            //Failure cases
+            assertFalse(scheduleScraper.retrieveExamSchedule("201209", null));
+            assertFalse(scheduleScraper.retrieveExamSchedule(null, finals1));
+            assertFalse(scheduleScraper.retrieveExamSchedule(null, null));
+            System.out.println("FAILURE TESTS PASSED!");
+
+            //far out of range case! (schedule 2)
+            assertFalse(scheduleScraper.retrieveExamSchedule("201409", finals2));
+            for (Course course : finals2)
+                assertTrue(course.toXML().equals(emptyCourse.toXML()));
+            System.out.println("FUTURE SEMESTERCODE TEST PASSED!");
+
+            //far before range case (schedule 3)
+            assertFalse(scheduleScraper.retrieveExamSchedule("200809", finals3));
+            for (Course course : finals3)
+                assertTrue(course.toXML().equals(emptyCourse.toXML()));
+            System.out.println("WAY BACK WHEN TEST PASSED!");
+
+            //Enrolled at VT but not semester
+            assertFalse(scheduleScraper.retrieveExamSchedule("201207", finals4));
+            for (Course course : finals4)
+                assertTrue(course.toXML().equals(emptyCourse.toXML()));
+            System.out.println("NON-PARTICIPATING SEMESTER TEST PASSED!");
+
+            //Totally invalid semesterCode (but correct length)
+            assertFalse(scheduleScraper.retrieveExamSchedule("548766", finals5));
+            for (Course course : finals5)
+                assertTrue(course.toXML().equals(emptyCourse.toXML()));
+            System.out.println("INVALID SEMESTER CODE TEST PASSED!");
+
+            //Wrong length semesterCode
+            assertFalse(scheduleScraper.retrieveExamSchedule("20120999", finals6));
+            for (Course course : finals6)
+                assertTrue(course.toXML().equals(emptyCourse.toXML()));
+            System.out.println("TOO LONG SEMESTER CODE TEST PASSED!");
+
+            assertTrue(cas.isActive());
+            //Additional Times course
+            System.out.println("\n\nADDITIONAL TIMES CHECK!!!!!!!!");
+            assertTrue(scheduleScraper.retrieveExamSchedule("201109", finals7));
+            for (Course course : finals7)
+                System.out.println(course.toXML());
+            System.out.println("********************************************************");
+            System.out.println("********************************************************");
+            for (Course course : finals7)
+                System.out.println(course.toString());
+            System.out.println("\nEND\nEND ADDITIONAL TIMES CHECK!!!!!!!!");
+        }
+        catch (WrongLoginException e) {
+            e.printStackTrace();
+        }
+        catch (HokieSpaTimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String username = "";
+    String password = "";
+}
